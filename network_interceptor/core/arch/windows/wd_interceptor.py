62c132acdfb7874d1d5ce536d2881adc30aaa6f6
Nemoumbra <garbuzov.nickolay@gmail.com>
2024-12-12 16:54:43 +0300

    WD filter validation + WD packet replacement added

--- network_interceptor/core/arch/windows/wd_interceptor.py
+++ network_interceptor/core/arch/windows/wd_interceptor.py
@@ -2,6 +2,7 @@
 from scapy.packet import Packet
 from scapy.layers.inet import IP
 from pydivert import WinDivert
+from pydivert import Packet as WDPacket
 
 from network_interceptor.core.intercepted_packet import InterceptedPacket
 from network_interceptor.core.interceptor import BaseInterceptor
@@ -32,31 +33,39 @@ class WinDivertInterceptor(BaseInterceptor):
 
         self._wd_filter = wd_config["filter"]
 
+        # Do some validation here...
+        res, pos, msg = WinDivert.check_filter(self._wd_filter)
+        if res:
+            return
+        raise ValueError(
+            f"Wrong 'filter' setting format for 'windivert': {msg}. Error is at {pos} ('{self._wd_filter[pos:]}')"
+        )
+
+    def _build_new_packet(self, old_packet: WDPacket, new_packet: Packet):
+        pkt = WDPacket(new_packet.build(), old_packet.interface, old_packet.direction)
+        return pkt
 
     def _run_impl(self):
         try:
             with WinDivert(self._wd_filter) as wd:
                 for pkt in wd:
-                    try:
-                        scapy_packet = IP(pkt.raw.tobytes())
-                        self._handle_packet(scapy_packet)
+                    scapy_packet = IP(pkt.raw.tobytes())
+                    old_size = len(scapy_packet)
+                    self._handle_packet(scapy_packet)
 
-                        if not self._action_taken:
-                            # The packet is accepted
-                            wd.send(pkt)
-                            continue
+                    if not self._action_taken:
+                        # The packet is accepted
+                        wd.send(pkt, True)
+                        continue
 
-                        if self._new_packet is not None:
-                            # The packet is replaced
-                            # TODO: serialize the new packet into WinDivert format
-                            wd.send(pkt)
-                            continue
+                    if self._new_packet is not None:
+                        # The packet is replaced
+                        new_size = len(self._new_packet)
+                        new_pkt = self._build_new_packet(pkt, self._new_packet)
+                        wd.send(new_pkt, True)
+                        continue
 
-                        # The remaining case is "drop". It doesn't require special handling
+                    # The remaining case is "drop". It doesn't require special handling
 
-                    except OSError as e:
-                        print(f"OS error: {e}")
-                    except RuntimeError as e:
-                        print(f"Runtime error: {e}")
         except KeyboardInterrupt:
             return
